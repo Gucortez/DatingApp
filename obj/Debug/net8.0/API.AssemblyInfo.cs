f3e8b43e9c5dc4d1c7ca81a4b4bd36af515c3523
Gucortez <guhcortez@icloud.com>
2024-08-20 20:48:24 -0600

    login method creation

--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88c0a922c8a57d6317fa8926d5af760717721585")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9db3b956b07bf9a23829d5d545139acacfbdd285")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
