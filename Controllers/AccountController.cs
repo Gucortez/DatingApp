9db3b956b07bf9a23829d5d545139acacfbdd285
Gucortez <guhcortez@icloud.com>
2024-08-20 20:05:57 -0600

    password and account register

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Text;
+using System.Security.Cryptography;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using API.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+public class AccountController(DataContext context) : BaseApiController
+{
+    [HttpPost("register")] //account/register
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto) 
+    {
+        if(await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+
+        using var hmac = new HMACSHA512();
+
+        var user = new AppUser
+        {
+            UserName = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PasswordSalt = hmac.Key
+
+        };
+
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+
+        return user;
+    }
+
+    private async Task<bool> UserExists(string username)
+    {
+        return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
+    }
+
+
+}
+
--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4209cad585a091bd87548d19c7992ea6709c2929")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88c0a922c8a57d6317fa8926d5af760717721585")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
