f0f1833ad350dabecfd897341b6f5b4559e464e8
Gucortez <guhcortez@icloud.com>
2024-08-28 20:48:35 -0600

    error handling

--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+899aa156c3c618932911603105f09f06390c07bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5bcd9b3090d362bdec9f0de9604bc809f865ef8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
